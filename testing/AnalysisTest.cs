9645ae4b518abd30a60edeb77188d6433214ce6f
Nick Diplos <nickd321@gmail.com>
2023-12-04 17:32:18 +0000

    added more avgword length testing

--- testing/AnalysisTest.cs
+++ testing/AnalysisTest.cs
@@ -11,8 +11,24 @@ public class AnalysisTest
 
         var expected = 4;
 
-        var output = analysisTest.AvgWordLength();
+        var output = analysisTest.AvgWordCount;
         
         Assert.Equal(expected, output);
     }
+    
+    [Fact]
+    public void AvgWordLength_ReturnsTheWordLengthForMultipleWords()
+    {
+        List<string> testWords = new List<string> { "This", "is", "a", "test" };
+        
+        var analysisTest =  new Analysis(testWords);
+
+        var expected = ("This".Length + "is".Length + "a".Length + "test".Length) / (float) 4;
+
+        var output = analysisTest.AvgWordCount;
+        
+        Assert.Equal(expected, output);
+    }
+    
+    
 }
