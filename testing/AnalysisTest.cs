b8df635e6708d501a987478ef38f8fd4c0c0f624
Nick Diplos <nickd321@gmail.com>
2023-12-06 17:59:22 +0000

    reconfigured tests in line with new analysis methods

--- testing/AnalysisTest.cs
+++ testing/AnalysisTest.cs
@@ -1,17 +1,17 @@
 using dataanalysis;
 
-public class AnalysisTest
+public class SingleThreadTest
 {
     [Fact]
     public void AvgWordLength_ReturnsTheWordLengthForOneWord()
     {
         List<string> testWords = new List<string> { "Test" };
         
-        var analysisTest =  new Analysis(testWords);
+        var analysisTest =  new SingleThreadProcessor(testWords);
 
         var expected = 4;
 
-        var output = analysisTest.AvgWordCount;
+        var output = analysisTest.GetMean();
         
         Assert.Equal(expected, output);
     }
@@ -21,11 +21,11 @@ public class AnalysisTest
     {
         List<string> testWords = new List<string> { "This", "is", "a", "test" };
         
-        var analysisTest =  new Analysis(testWords);
+        var analysisTest =  new SingleThreadProcessor(testWords);
 
         var expected = ("This".Length + "is".Length + "a".Length + "test".Length) / (float) 4;
 
-        var output = analysisTest.AvgWordCount;
+        var output = analysisTest.GetMean();
         
         Assert.Equal(expected, output);
     }
