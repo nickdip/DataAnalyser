2863ccfb270a454a864e845a1d8b7fc50bfb310b
Nick Diplos <nickd321@gmail.com>
2023-12-07 15:18:58 +0000

    fixed issues with analysis testing not passing

--- testing/AnalysisTest.cs
+++ testing/AnalysisTest.cs
@@ -23,7 +23,7 @@ public class SingleThreadTest
         
         var analysisTest =  new SingleThreadProcessor(testWords);
 
-        var expected = ("This".Length + "is".Length + "a".Length + "test".Length) / (float) 4;
+        var expected = ("This".Length + "is".Length + "a".Length + "test".Length) / (float) testWords.Count;
 
         var output = analysisTest.GetMean();
         
@@ -31,4 +31,53 @@ public class SingleThreadTest
     }
     
     
+}
+
+public class MultiThreadTest
+{
+    [Fact]
+    public void AvgWordLength_ReturnsTheWordLengthForOneWord()
+    {
+        List<string> testWords = new List<string> { "Test" };
+        
+        var analysisTest =  new MultiThreadingProcessor(testWords);
+
+        var expected = 4;
+
+        var output = analysisTest.Mean();
+        
+        Assert.Equal(expected, output);
+    }
+    
+    [Fact]
+    public void AvgWordLength_ReturnsTheWordLengthForMultipleWords()
+    {
+        List<string> testWords = new List<string> { };
+
+        var random = new Random();
+        
+        for (int i = 0; i < 100000; i++)
+        {
+            int randomNumber = random.Next(0, 1000000);
+            testWords.Add(randomNumber.ToString());
+        }
+        
+        var analysisTest =  new MultiThreadingProcessor(testWords);
+
+
+        float expected = 0;
+        
+        foreach (string numberword in testWords)
+        {
+            expected += (float) numberword.Length / testWords.Count;
+        }
+
+        double expectedRounded = Math.Round(expected, 1);
+        
+        var output = analysisTest.Mean();
+        
+        double outputRounded = Math.Round(output, 1);
+        
+        Assert.Equal(expectedRounded, outputRounded);
+    }
 }
